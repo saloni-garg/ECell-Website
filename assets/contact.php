e8c0067965c512021838fbcca0f9bd3a752418e4
Saloni Garg <salonigarg2310@gmail.com>
2017-05-30 06:31:52 -0200

    Update contact.php

--- assets/contact.php
+++ assets/contact.php
@@ -8,7 +8,7 @@ function isEmail($email) {
 if($_POST) {
 
     // Enter the email where you want to receive the message
-    $emailTo = 'contact.azmind@gmail.com';
+    $emailTo = 'salonigarg2310@gmail.com';
 
     $clientEmail = addslashes(trim($_POST['email']));
     $subject = addslashes(trim($_POST['subject']));
